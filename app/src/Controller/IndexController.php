9ed17e585663b2bbf2b18932a859c1e2f59ea6be
bailey lievens <baileylievens@hotmail.be>
2023-01-25 00:09:04 +0100

    Feat: Added basic controller

--- app/src/Controller/IndexController.php
+++ app/src/Controller/IndexController.php
@@ -2,16 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController
+class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return new Response(
-            '<html><body><h1>Hello Symfony6!</h1></body></html>'
-        );
+        return $this->render('base.html.twig');
     }
 }
